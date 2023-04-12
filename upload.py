ad09ba3578ad2560be7974c7cb04289abf64c47a
ooyarce <oioyarce@miuandes.cl>
2023-04-12 13:35:14 -0400

    Commit realizado el 12/04/2023 a las 13:35:14

--- upload.py
+++ upload.py
@@ -1,16 +1,17 @@
 import logging
-
-logging.basicConfig(filename='github_uploads.log', level=logging.INFO)
-
-# Resto del código aquí
 import subprocess
 import datetime
 import time
+
 """
 Puedes revisar los registros de systemd para ver las salidas del script. Puedes usar el siguiente comando para ver los registros de systemd:
 > journalctl -u upload.service
 Este comando mostrará el registro de systemd para el servicio upload.service, lo que incluirá las salidas del comando print() en el script.
 """
+
+# Configurar el registro de salida
+logging.basicConfig(filename='github_uploads.log', level=logging.INFO)
+
 # Definir la duración del intervalo entre commits (en segundos)
 intervalo = 3600
 
@@ -29,12 +30,12 @@ while True:
     subprocess.run(['git', 'push'])
 
     # Imprimir mensaje de confirmación
-    print(f'Se ha realizado un commit a las {fecha_actual.strftime("%H:%M:%S")}')
+    logging.info(f'Se ha realizado un commit a las {fecha_actual.strftime("%H:%M:%S")}')
 
     # Esperar hasta el siguiente intervalo
     tiempo_restante = intervalo
     while tiempo_restante > 0:
-        print(f'Faltan {tiempo_restante//60} minutos y {tiempo_restante%60} segundos para el siguiente commit')
+        logging.info(f'Faltan {tiempo_restante//60} minutos y {tiempo_restante%60} segundos para el siguiente commit')
         tiempo_espera = min(tiempo_restante, 600)  # Esperar un máximo de 10 minutos
         time.sleep(tiempo_espera)
         tiempo_restante -= tiempo_espera
