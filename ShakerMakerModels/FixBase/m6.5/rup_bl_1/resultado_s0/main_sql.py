8e31f6cac8d4441ad3d34e35f6e8e7fb6fcb74cc
ooyarce <oioyarce@miuandes.cl>
2023-04-12 23:53:38 -0400

    Commit realizado el 12/04/2023 a las 23:53:38

--- ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/main_sql.py
+++ ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/main_sql.py
@@ -9,8 +9,9 @@ clustername = 'Omar-Ubuntu'
 comments = 'Just testing model_benchmark function'
 model_benchmark(clustername,comments)
 """
-bsunits, absacc_units,rel_displ_units
-model_structure_perfomance()
+bsunits, absacc_units, rel_displ_units, maxbs_units, maxd_units = 'kN', 'm/s/s', 'm', 'kN', 'm'
+
+model_structure_perfomance(bsunits, absacc_units, rel_displ_units, maxbs_units, maxd_units)
 #close sql
 cursor.close()
 cnx.close()
