6061343a2e52d6cca7fc13222ea42e959cc685f7
ooyarce <oioyarce@miuandes.cl>
2023-04-13 16:10:02 -0400

    Commit realizado el 13/04/2023 a las 16:10:02

--- ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/temp.py
+++ ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/temp.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from scipy.interpolate import make_interp_spline
 # Valores de las variables
 sobrelendig = [4, 4, 4, 5, 5, 6, 7, 8, 9, 9, 9]
 mercado_inmobiliario = [6, 6, 7, 8, 8, 7, 6, 5, 4, 4, 5]
@@ -8,10 +8,10 @@ credito_hipotecario = [8, 8, 7, 6, 6, 7, 8, 9, 10, 10, 9]
 derivados_financieros = [4, 4, 4, 5, 6, 7, 8, 9, 10, 10, 9]
 regulacion = [5, 5, 5, 5, 6, 7, 8, 8, 8, 9, 9]
 riesgo_crediticio = [8, 8, 8, 9, 9, 10, 10, 10, 9, 8, 8]
-securitizacion = [6, 6, 7, 8, 8, 7, 6, 5, 4, 4, 5]
+securitizacion = [5, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4]
 cdos = [7, 7, 8, 9, 9, 10, 10, 9, 8, 8, 9]
-interes_bajo = [1, 1, 1, 1, 1, 1, 1, 1, 1, 5.5, 5.5]
-crisis_financiera = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+interes_bajo = [1, 1, 1, 1, 1, 1, 1, 1, 1, 5.5, 6.5]
+crisis_financiera = [0, 0, 0, 0, 1, 1, 1, 2, 3, 4, 1]
 
 # Tiempo
 time = np.arange(2001, 2012)
@@ -20,26 +20,48 @@ time = np.arange(2001, 2012)
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 12))
 plt.subplots_adjust(hspace=0.5)
 
+
 # Variable Sobrelendig
-ax1.plot(time, sobrelendig, label="Sobrelendig")
+spl = make_interp_spline(time, sobrelendig)
+x_new = np.linspace(time.min(), time.max(), 100)
+y_new = spl(x_new)
+ax1.plot(x_new, y_new, label="Sobrelendig")
 ax1.set_ylabel("Monto del préstamo (en miles de dólares)")
 ax1.set_title("Variables financieras durante la crisis del 2008")
 ax1.legend()
 
 # Variables de mercado inmobiliario
-ax2.plot(time, mercado_inmobiliario, label="Mercado Inmobiliario")
-ax2.plot(time, credito_hipotecario, label="Crédito Hipotecario")
+spl = make_interp_spline(time, mercado_inmobiliario)
+spl2 = make_interp_spline(time, credito_hipotecario)
+y1 = spl(x_new)
+y2 = spl2(x_new)
+ax2.plot(x_new, y1, label="Mercado Inmobiliario")
+ax2.plot(x_new, y2, label="Crédito Hipotecario")
 ax2.set_ylabel("Monto (en miles de dólares)")
 ax2.legend()
 
 # Variables de riesgo y regulación
-ax3.plot(time, derivados_financieros, label="Derivados Financieros")
-ax3.plot(time, regulacion, label="Regulación")
-ax3.plot(time, riesgo_crediticio, label="Riesgo Crediticio")
-ax3.plot(time, securitizacion, label="Securitización")
-ax3.plot(time, cdos, label="CDOs")
-ax3.plot(time, interes_bajo, label="Interés Bajo")
-ax3.plot(time, crisis_financiera, label="Crisis Financiera")
+spl1 = make_interp_spline(time,derivados_financieros)
+spl2 = make_interp_spline(time,regulacion)
+spl3 = make_interp_spline(time,riesgo_crediticio)
+spl4 = make_interp_spline(time,securitizacion)
+spl5 = make_interp_spline(time,cdos)
+spl6 = make_interp_spline(time,interes_bajo)
+spl7 = make_interp_spline(time,crisis_financiera)
+y1 = spl1(x_new)
+y2 = spl2(x_new)
+y3 = spl3(x_new)
+y4 = spl4(x_new)
+y5 = spl5(x_new)
+y6 = spl6(x_new)
+y7 = spl7(x_new)
+ax3.plot(x_new, y1, label="Derivados Financieros")
+ax3.plot(x_new, y2, label="Regulación")
+ax3.plot(x_new, y3, label="Riesgo Crediticio")
+ax3.plot(x_new, y4, label="Securitización")
+ax3.plot(x_new, y5, label="CDOs")
+ax3.plot(x_new, y6, label="Interés Bajo")
+ax3.plot(x_new, y7, label="Crisis Financiera")
 ax3.set_ylabel("Monto (logarítmico)")
 ax3.set_xlabel("Año")
 ax3.legend(loc='upper right', bbox_to_anchor=(1, 1))
