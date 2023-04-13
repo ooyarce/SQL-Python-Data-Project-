306aa66e73b2a3677dfe907199fad75af8d70d75
ooyarce <oioyarce@miuandes.cl>
2023-04-13 15:29:49 -0400

    Commit realizado el 13/04/2023 a las 15:29:49

--- ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/temp.py
+++ ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/temp.py
@@ -1,22 +1,49 @@
-import pandas as pd
+import matplotlib.pyplot as plt
 
-# Crear un DataFrame de ejemplo con 5 filas y 10 columnas
-datos = pd.DataFrame({'col1': [1, 2, 3, 4, 5],
-                      'col2': [6, 7, 8, 9, 10],
-                      'col3': [11, 12, 13, 14, 15],
-                      'col4': [16, 17, 18, 19, 20],
-                      'col5': [21, 22, 23, 24, 25],
-                      'col6': [26, 27, 28, 29, 30],
-                      'col7': [31, 32, 33, 34, 35],
-                      'col8': [36, 37, 38, 39, 40],
-                      'col9': [41, 42, 43, 44, 45],
-                      'col10': [46, 47, 48, 49, 50]})
+# Valores de cada variable a lo largo del tiempo
+sobrelendig = [120, 125, 130, 135, 140, 145, 150, 155, 160]
+mercado_inmobiliario = [100, 110, 120, 130, 140, 150, 160, 150, 140, 130]
+credito_hipotecario = [80, 85, 90, 92, 94, 95, 93, 90, 85, 80]
+derivados_financieros = [200, 225, 250, 275, 300, 325, 350, 300, 250, 200]
+regulacion = [0, 0, 0, 0, 0, 10, 50, 100, 150, 200]
+riesgo_crediticio = [1, 2, 3, 4, 5, 6, 8, 10, 12, 10]
+securitizacion = [50, 60, 70, 80, 90, 100, 80, 60, 40, 20]
+cdos = [100, 125, 150, 175, 200, 225, 250, 200, 150, 100]
+interes_bajo = [1.25, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.25]
+crisis_financiera = [1200, 1250, 1300, 1350, 1400, 1350, 1300, 1250, 1200, 1250]
 
-# Seleccionar las cuatro columnas de interés por su nombre
-cols_interes = ['col3', 'col4', 'col7', 'col9']
-datos_interes = datos[cols_interes]
+# Intervalo de tiempo
+anios = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010]
 
-# Calcular el promedio de los valores de las cuatro columnas para cada fila
-promedio_filas = datos_interes.mean(axis=1)
+# Crear figura y subplots
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 15))
 
-print(promedio_filas)
+# Gráfico 1 - Unidades en cantidades
+ax1.plot(anios, sobrelendig, label='Sobrelendig')
+ax1.plot(anios, mercado_inmobiliario, label='Mercado inmobiliario')
+ax1.plot(anios, credito_hipotecario, label='Crédito hipotecario')
+ax1.plot(anios, derivados_financieros, label='Derivados financieros')
+ax1.set_xlabel('Años')
+ax1.set_ylabel('Unidades')
+ax1.set_title('Variables relacionadas con la crisis financiera del 2008 - Unidades en cantidades')
+
+# Gráfico 2 - Unidades en dólares
+ax2.plot(anios, regulacion, label='Regulación')
+ax2.plot(anios, riesgo_crediticio, label='Riesgo crediticio')
+ax2.plot(anios, securitizacion, label='Securitización')
+ax2.set_xlabel('Años')
+ax2.set_ylabel('Unidades')
+ax2.set_title('Variables relacionadas con la crisis financiera del 2008 - Unidades en dólares')
+
+# Gráfico 3 - Unidades en miles de millones de dólares
+ax3.plot(anios, cdo, label='CDOs')
+ax3.set_xlabel('Años')
+ax3.set_ylabel('Unidades')
+ax3.set_title('Variables relacionadas con la crisis financiera del 2008 - Unidades en miles de millones de dólares')
+
+# Ajustar leyenda y mostrar gráfico
+ax1.legend()
+ax2.legend()
+ax3.legend()
+plt.tight_layout()
+plt.show()
