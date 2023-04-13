c807dae1095496d06bc2a737444ccf59f1243a3d
ooyarce <oioyarce@miuandes.cl>
2023-04-13 13:49:15 -0400

    Commit realizado el 13/04/2023 a las 13:49:15

--- ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/main_sql.py
+++ ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/main_sql.py
@@ -9,7 +9,20 @@ clustername = 'Omar-Ubuntu'
 comments = 'Just testing model_benchmark function'
 model_benchmark(clustername,comments)
 """
-
+parameters = {
+    'bs_units'       : 'kN',                                                            
+    'max_bs_units'   : 'kN',                                                                
+    'rel_displ_units': 'm',             
+    'max_drift_units': 'm',             
+    'abs_acc_units'  : 'm/s/s',         
+    'model_name'     : 'FixBaseV3',     
+    'clustername'    : 'Esmeralda HPC Cluster by jaabell@uandes.cl',
+    'perf_comments'  : 'Comments for model_structure_perfomance',
+    'specs_comments' : 'Comments for model_specs_structure',
+    'sim_comments'   : 'Comments for simulation_model',
+    'bench_comments' : 'Comments for model_benchmark',
+    'linearity'      : 1
+}
 bsunits, absacc_units, rel_displ_units, maxbs_units, maxd_units = 'tonf', 'cm/hrs/hrs', 'in', 'kgf', 'km'
 model_structure_perfomance(bsunits, absacc_units, rel_displ_units, maxbs_units, maxd_units)
 
