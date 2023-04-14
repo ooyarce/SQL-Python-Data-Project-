8ea0241635248e48d3ae3e52e73461a5c4f33a65
ooyarce <oioyarce@miuandes.cl>
2023-04-14 17:04:07 -0400

    Commit realizado el 14/04/2023 a las 17:04:07

--- ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/sql_functions.py
+++ ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/sql_functions.py
@@ -80,7 +80,10 @@ def simulation_sm_input():
 		raise TypeError('Folders name are not following the format rup_[bl/ns/sn]_[iteration].')
 	#get realization id
 	iteration = Rup_type = os.path.dirname(__file__).split('/')[-2].split('_')[2]
-	print(iteration)
+	
+	#get location
+	location = os.path.dirname(__file__).split('/')[-1].split('_')[1]
+	print(location)
 
 def simulation_model(model_name = '', model_comments = '', bs_units='', abs_acc_units='', rel_displ_units='', max_bs_units='', max_drift_units='', perf_comments = '',  linearity = 1, specs_comments = '', clustername = '',bench_comments = ''):
 	model_benchmark(clustername,bench_comments)
