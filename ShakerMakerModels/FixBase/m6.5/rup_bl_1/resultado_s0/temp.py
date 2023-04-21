5660a3fc82554756a59be5833e133837a1154961
ooyarce <oioyarce@gmail.com>
2023-04-21 11:28:01 -0400

    Windows

--- ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/temp.py
+++ ShakerMakerModels/FixBase/m6.5/rup_bl_1/resultado_s0/temp.py
@@ -1,6 +1,35 @@
-import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
-fecha_actual = datetime.datetime.now()
-fecha_actual = fecha_actual.strftime("%B %d, %Y")
+# Definir los parámetros para la zona 2 y tipo de suelo D
+a = 0.4
+T1 = 0.1
+T2 = 0.5
+T3 = 2.0
+T4 = 10.0
+pga = 0.25
 
-print(fecha_actual)
+# Definir los valores de período y aceleración
+T = np.logspace(-2, 1, 100)
+S = np.zeros_like(T)
+
+# Calcular el espectro de respuesta sísmica
+for i, t in enumerate(T):
+    if t <= T1:
+        S[i] = a * (0.4 + (0.6 * t / T1))
+    elif T1 < t <= T2:
+        S[i] = a
+    elif T2 < t <= T3:
+        S[i] = a * (T2 / t)
+    elif T3 < t <= T4:
+        S[i] = (0.4 - (0.1 * np.log10(t / T3))) * pga
+    else:
+        S[i] = (0.4 - (0.1 * np.log10(T4 / T3))) * pga * (T4 / t) ** 2
+
+# Graficar el espectro de respuesta sísmica
+plt.loglog(T, S)
+plt.xlabel('Período (s)')
+plt.ylabel('Aceleración (g)')
+plt.title('Espectro de respuesta sísmica para zona 2, tipo de suelo D')
+plt.grid(True, which="both")
+plt.show()
