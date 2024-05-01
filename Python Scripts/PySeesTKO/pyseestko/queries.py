8270b8f0a0f25bc5ab8bc3680a2ba0ecc0ceeb4d
ooyarce <oioyarce@miuandes.cl>
2024-05-01 16:33:50 -0400

    minor fixes6

--- Python Scripts/PySeesTKO/pyseestko/queries.py
+++ Python Scripts/PySeesTKO/pyseestko/queries.py
@@ -6,10 +6,119 @@ from pyseestko.plotting   import Plotting               #type: ignore
 from pyseestko.utilities  import NCh433_2012            #type: ignore
 from pyseestko.utilities  import initialize_ssh_tunnel  #type: ignore
 from pathlib              import Path
+from typing               import List, Dict, Tuple 
 import pandas as pd
 import pickle
 import time
 
+# ==================================================================================================
+# MAIN FUNCTION
+# ==================================================================================================
+"""
+This function will execute the main query to get the results from the database
+The logic is the following:
+1. Iterate over the simulation types
+2. Iterate over the stations
+3. Iterate over the iterations
+4. Iterate over the number of substructures
+5. Query the database
+6. Append the results to the lists
+7. Return the lists
+
+Params:
+- sim_types: List of simulation types
+- stations: List of stations
+- iterations: List of iterations
+- nsubs_lst: List of number of substructures
+- mag_map: Dictionary with the mapping of the magnitudes
+- loc_map: Dictionary with the mapping of the locations
+- rup_map: Dictionary with the mapping of the rupture types
+- user: User of the database
+- password: Password of the database
+- host: Host of the database
+- database: Name of the database
+
+Optional params:
+- linearity: Linearity of the model, always 1 for linear, 2 for non-linear, default is 1
+- stories: Number of stories, default is 20
+- magnitude: Magnitude of the earthquake, default is 6.7
+- rupture_type: Type of rupture, default is 1
+- save_drift: Path to save the drift plots if None, it will not save the plots nor the data
+- save_spectra: Path to save the spectra plots, if None, it will not save the plots nor the data
+- save_b_shear: Path to save the base shear plots, if None, it will not save the plots nor the data
+- windows: True if the OS is Windows, False if Linux
+
+Returns:
+- drifts_df_lst: List of drift dataframes
+- spectra_df_lst: List of spectra dataframes
+- base_shear_df_lst: List of base shear dataframes
+
+Note:
+This is mean to be used for statistical analysis, so this is the main fuction to access to the specific data
+and then analyze it with diverse statistical methods such as ANOVA, POWER ANALYSIS OR MANOVA.
+"""
+def executeMainQuery(
+    # Params
+    sim_types   : List[int], 
+    stations    : List[int], 
+    iterations  : List[int], 
+    nsubs_lst   : List[int], 
+    mag_map     : Dict[int, str], 
+    loc_map     : Dict[int, str], 
+    rup_map     : Dict[int, str], 
+    user        : str, 
+    password    : str, 
+    host        : str, 
+    database    : str,
+    
+    # Optional params
+    linearity   : int  = 1, 
+    stories     : int  = 20, 
+    magnitude   : int  = 6.7, 
+    rupture_type: int  = 1, 
+    save_drift  : str  = None, 
+    save_spectra: str  = None, 
+    save_b_shear: str  = None, 
+    windows     : bool = True):
+    
+    # Iterative params
+    drifts_df_lst     = []
+    spectra_df_lst    = []
+    base_shear_df_lst = []
+
+    # Iterate over the subs, then over the sim_type and then over the stations so we can get all the results
+    for sim_type in sim_types:
+        for station in stations:
+            for iteration in iterations:
+                for nsubs in nsubs_lst:
+                    structure_weight = 22241.3 if nsubs == 4 else 18032.3
+                    # Init the classes
+                    plotter = Plotting(sim_type, stories,                    # The class that plots the data
+                                    nsubs, magnitude,
+                                    iteration, rupture_type,
+                                    station)
+                    query   = ProjectQueries(user, password, host, database, # The class that queries the database
+                                            sim_type, linearity,
+                                            mag_map.get(magnitude,    'None'),
+                                            rup_map.get(rupture_type, 'None'), iteration,
+                                            loc_map.get(station,      'None'),
+                                            stories, nsubs, plotter, windows=windows)
+                    
+                    # Get the results for zone = 'Las Condes', soil_category = 'B' and importance = 2
+                    drift, spectra, base_shear = query.getAllResults(save_drift, 
+                                                                    save_spectra, 
+                                                                    save_b_shear, 
+                                                                    structure_weight)
+                    # Append the results to the lists
+                    drifts_df_lst.append(drift)
+                    spectra_df_lst.append(spectra)
+                    base_shear_df_lst.append(base_shear)
+    print('Done!')
+    return drifts_df_lst, spectra_df_lst, base_shear_df_lst
+
+# ==================================================================================================
+# CLASS TO QUERY THE DATABASE
+# ==================================================================================================
 class ProjectQueries:
     def __init__(
         self,
@@ -67,7 +176,7 @@ class ProjectQueries:
         max_center_x = pickle.loads(structure_max_drift_per_floor[3]) # type: ignore
         max_center_y = pickle.loads(structure_max_drift_per_floor[4]) # type: ignore
 
-        return max_corner_x, max_corner_y, max_center_x, max_center_y
+        return max_center_x, max_center_y, max_corner_x, max_corner_y
 
     def stories_spectra(self):
         query = """
@@ -127,26 +236,32 @@ class ProjectQueries:
         save_spectra     :str|None,
         save_b_shear     :str|None,
         structure_weight :float,
-        zone             :str,
-        soil_category    :str,
-        importance       :int
+        zone             :str = 'Las Condes',
+        soil_category    :str = 'B',
+        importance       :int = 2
                       ):
         # Init params
         start_time = time.time()
-        drift_results_df   = pd.DataFrame(columns=['CM x', 'CM y', 'Max x', 'Max y']).rename_axis('Time Step')
-        spectra_results_df = None
+        drift_results_df      = None
+        spectra_results_df    = None
+        base_shear_results_df = None
         
         # ===================================================================================================
         # ==================================== QUERY THE DRIFT PER FLOOR ====================================
         # ===================================================================================================
         if save_drift is not None:
             # Init the query
-            max_corner_x, max_corner_y, max_center_x, max_center_y = self.story_drift()
+            max_center_x, max_center_y, max_corner_x, max_corner_y = self.story_drift()
 
             # Plot the data
             self.plotter.save_path = Path(save_drift)
             ax                = self.plotter.plotModelDrift(max_corner_x, max_center_x, max_corner_y, max_center_y)
-            drift_results_df  = pd.DataFrame({'CM x': max_center_x, 'CM y': max_center_y, 'Max x': max_corner_x, 'Max y': max_corner_y}, index=range(1, len(max_corner_x)+1)).rename_axis('Time Step')
+            drift_results_df  = pd.DataFrame({'CM x': max_center_x, 
+                                              'CM y': max_center_y, 
+                                              'Max x': max_corner_x, 
+                                              'Max y': max_corner_y}, 
+                                             index=range(1, len(max_corner_x)+1)).rename_axis('Story')
+            
 
         # ===================================================================================================
         # ===================================== QUERY THE STORY SPECTRUM ====================================
@@ -160,7 +275,9 @@ class ProjectQueries:
             stories_lst = [1,5,10,15,20]
             ax, T, Spx = self.plotter.plotLocalStoriesSpectrums(accel_df, story_nodes_df, 'x', stories_lst, soften=True)
             ax, _, Spy = self.plotter.plotLocalStoriesSpectrums(accel_df, story_nodes_df, 'y', stories_lst, soften=True)
-            spectra_results_df = pd.DataFrame({'Spectrum X': Spx, 'Spectrum Y': Spy}, index=T).rename_axis('Period [T]')
+            spectra_results_df = pd.DataFrame({'Spectrum X': Spx, 
+                                               'Spectrum Y': Spy}, 
+                                              index=T).rename_axis('Period [T]')
 
 
         # ===================================================================================================
@@ -179,6 +296,10 @@ class ProjectQueries:
             self.plotter.save_path = Path(save_b_shear)
             ax = self.plotter.plotShearBaseOverTime(time_series, shear_x, Qmin, Qmax, 'x')
             ax = self.plotter.plotShearBaseOverTime(time_series, shear_y, Qmin, Qmax, 'y')
+            base_shear_results_df = pd.DataFrame({'Shear X': shear_x, 
+                                                  'Shear Y': shear_y, 
+                                                  'Shear Z': shear_z}, 
+                                                 index=time_series).rename_axis('Time Step')
 
         # ===================================================================================================
         # =========================================== END QUERIES ===========================================
@@ -186,6 +307,7 @@ class ProjectQueries:
         end_time = time.time()
         self.close_connection()
         print(f'Elapsed time: {end_time - start_time} seconds.')
+        return drift_results_df, spectra_results_df, base_shear_results_df
         
 
 
--- Python Scripts/query_data.py
+++ Python Scripts/query_data.py
@@ -2,9 +2,8 @@
 # ================================== INIT AND CONNECT TO DATABASE ==================================
 # ==================================================================================================
 # Import modules
-from pyseestko.queries    import ProjectQueries            #type: ignore
-from pyseestko.utilities  import get_mappings #type: ignore
-from pyseestko.plotting   import Plotting                  #type: ignore
+from pyseestko.utilities  import get_mappings              #type: ignore
+from pyseestko.queries    import executeMainQuery          #type: ignore
 
 # DataBase user params
 user     = 'omarson'
@@ -12,7 +11,10 @@ password = 'Mackbar2112!'
 host     = 'localhost'
 database = 'stkodatabase'
 
-# Init Parameters
+
+# ==================================================================================================
+# ========================================== INPUT PARAMS ==========================================
+# ==================================================================================================
 """
 sim_types: 1 = FB, 2 = AB, 3 = DRM
 linearity: 1 = Linear, 2 = Non-Linear
@@ -22,43 +24,41 @@ stations: Can be any number from 0 to 9, -1 for not defined
 stories: For the moment, it can be 20 or 55
 nsubs_lst: For the moment, it can be 2 or 4
 """
-magnitude    = 6.7      # Constant
-rupture_type = 1        # Constant       
-linearity    = 1        # Constant
-stories      = 20       # Constant      
-nsubs_lst    = [4]      # 2,4
-sim_types    = [3]      # 1 = FB, 2 = AB, 3 = DRM
-iterations   = [1,2]#,4,5,6,7,8,9,10]      # 1,2,3,4,5,6,7,8,9,10
-stations     = [0,1,2,3,4,5,6,7,8,9]
-mag_map, loc_map, rup_map = get_mappings()
+# Define the parameters
+sim_types    = [i for i in range(3,4)]  # 1 = FB, 2 = AB, 3 = DRM
+nsubs_lst    = [i for i in range(4,5)]  # Can be: 2,4
+iterations   = [i for i in range(1,11)] # Can be: {1,2,3,4,5,6,7,8,9,10}
+stations     = [i for i in range(0,1)]  # Can be: {0,1,2,3,4,5,6,7,8,9 }
 
 # Define paths to save the plots
-save_drift       = 'C:/Users/oioya/OneDrive - miuandes.cl/Escritorio/Git-Updated/Thesis-Project-Simulation-Data-Analysis/DataBase-Outputs/Drift Outputs'
-save_spectra     = None#'C:/Users/oioya/OneDrive - miuandes.cl/Escritorio/Git-Updated/Thesis-Project-Simulation-Data-Analysis/DataBase-Outputs/Story Spectra Output'
-save_b_shear     = None#'C:/Users/oioya/OneDrive - miuandes.cl/Escritorio/Git-Updated/Thesis-Project-Simulation-Data-Analysis/DataBase-Outputs/Base Shear Output'
-structure_weight = 24531.7 # kN
-zone             = 'Las Condes'
-soil_category    = 'B'
-importance       = 2
+save_drift       = None #'C:/Users/oioya/OneDrive - miuandes.cl/Escritorio/Git-Updated/Thesis-Project-Simulation-Data-Analysis/DataBase-Outputs/Drift Outputs'
+save_spectra     = None #'C:/Users/oioya/OneDrive - miuandes.cl/Escritorio/Git-Updated/Thesis-Project-Simulation-Data-Analysis/DataBase-Outputs/Story Spectra Output'
+save_b_shear     = 'C:/Users/oioya/OneDrive - miuandes.cl/Escritorio/Git-Updated/Thesis-Project-Simulation-Data-Analysis/DataBase-Outputs/Base Shear Output'
 windows          = True # True if the OS is Windows, False if Linux
-# Iterate over the subs, then over the sim_type and then over the stations so we can get all the results
-for sim_type in sim_types:
-    for station in stations:
-        for iteration in iterations:
-            for nsubs in nsubs_lst:
-                # Init the classes
-                plotter = Plotting(sim_type, stories,                    # The class that plots the data
-                                   nsubs, magnitude,
-                                   iteration, rupture_type,
-                                   station)
-                query   = ProjectQueries(user, password, host, database, # The class that queries the database
-                                        sim_type, linearity,
-                                        mag_map.get(magnitude,    'None'),
-                                        rup_map.get(rupture_type, 'None'), iteration,
-                                        loc_map.get(station,      'None'),
-                                        stories, nsubs, plotter, windows=windows)
-                query.getAllResults(save_drift, save_spectra, save_b_shear, structure_weight, zone, soil_category, importance)
+mag_map, loc_map, rup_map = get_mappings()
 
+
+# ==================================================================================================
+# =========================================== QUERY DATA ===========================================
+# ==================================================================================================
+drifts_df_lst, spectra_df_lst, base_shear_df_lst = executeMainQuery(
+# Main params
+sim_types    = sim_types, 
+nsubs_lst    = nsubs_lst, 
+iterations   = iterations, 
+stations     = stations, 
+mag_map      = mag_map, 
+loc_map      = loc_map, 
+rup_map      = rup_map, 
+# DataBase params
+user         = user, 
+password     = password, 
+host         = host, 
+database     = database,
+# Save params
+save_drift   = save_drift, 
+save_spectra = save_spectra, 
+save_b_shear = save_b_shear)
 
 
 
