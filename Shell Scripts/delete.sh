5482c9d966830318e5ab2f6a91fc638cfe63c90f
ooyarce <oioyarce@miuandes.cl>
2024-05-03 13:10:04 -0400

    minor fixes7

--- Shell Scripts/delete.sh
+++ Shell Scripts/delete.sh
@@ -1,15 +1,29 @@
 # Iterar sobre cada número del 0 al 5
-for j in $(seq 1 2);do
+for j in $(seq 1 3);do
     echo "Entrando a rup_bl_$j"
     cd rup_bl_$j
-    for i in $(seq 3 9); do
+    for i in $(seq 0 9); do
         # Navegar a la carpeta correspondiente
         cd "station_s$i" || continue
 
         # Delete useless files
         rm -f *.mpco
         rm -f *.h5drm
+        rm -f *.mpco.cdata
+        rm analysis_steps.tcl
+        rm elements.tcl
+        rm main.tcl
+        rm materials.tcl
+        rm nodes.tcl
+        rm sections.tcl
+        rm import_h5py.py
+
+        # Results
+    	rm -rf Accelerations/*
+    	rm -rf Displacements/*
+    	rm -rf Reactions/*
         cd ..
+        
     done
     cd ..
 done
